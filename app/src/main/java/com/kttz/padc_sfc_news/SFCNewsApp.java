c19567213d26a8bc531c7888189ebaac8743ee93
Khine Theint Theint Zin <khinethienttheintzin@gmail.com>
2017-11-04 18:15:55 +0630

    Initialize skeleton Project with default structure / value.

---
+++ app/src/main/java/com/kttz/padc_sfc_news/SFCNewsApp.java
@@ -0,0 +1,18 @@
+package com.kttz.padc_sfc_news;
+
+import android.app.Application;
+import android.util.Log;
+
+/**
+ * Created by bingbing on 11/4/2017 AD.
+ */
+
+public class SFCNewsApp extends Application {
+
+    public static final String LOG_TAG="SECNewsApp";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
